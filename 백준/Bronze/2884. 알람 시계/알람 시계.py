333d1d1e60901ba351d5dce50095ceea159f9ba1
조정곤 <121420298+JeonggonCho@users.noreply.github.com>
2023-01-06 01:57:12 +0900

    [Bronze III] Title: 알람 시계, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -0,0 +1,9 @@
+H, M = input().split( )
+
+if int(M) >= 45:
+    print(int(H), (int(M) - 45))
+if int(M) < 45:
+    if int(H) == 0:
+        print(23, (60-(45-int(M))))
+    else:
+        print((int(H)-1), (60-(45-int(M))))
