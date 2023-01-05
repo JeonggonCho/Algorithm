ebd1756edeae9ed1d7cd0aa3de9d93925738a412
조정곤 <121420298+JeonggonCho@users.noreply.github.com>
2023-01-05 23:21:43 +0900

    [Bronze V] Title: 사분면 고르기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,13 @@
+num_x = int(input())
+num_y = int(input())
+
+if num_x > 0:
+    if num_y > 0:
+        print('1')
+    else:
+        print('4')
+else:
+    if num_y > 0:
+        print('2')
+    else:
+        print('3')
