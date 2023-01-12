bf849fe6964a8a7cbca08995721a4a1acb6a4b3e
조정곤 <121420298+JeonggonCho@users.noreply.github.com>
2023-01-12 13:53:17 +0900

    [D1] Title: 간단한 N 의 약수, Time: 88 ms, Memory: 42,440 KB -BaekjoonHub

--- SWEA/D1/1933. 간단한 N 의 약수/간단한 N 의 약수.py
+++ SWEA/D1/1933. 간단한 N 의 약수/간단한 N 의 약수.py
@@ -1,7 +1,7 @@
-N = int(input())
-list1 = []
+n = int(input())
+numbers = []
 
-for i in range(1, N+1):
-    if N % i == 0:
-        list1.append(i)
-print(*sorted(list1))
+for i in range(1, n+1):
+    if n % i == 0:
+        numbers.append(i)
+print(*sorted(numbers))
