fb12fdda31eb29a5d97aaa5d0547c546ca206649
조정곤 <121420298+JeonggonCho@users.noreply.github.com>
2023-01-15 11:37:51 +0900

    [D2] Title: 지그재그 숫자, Time: 91 ms, Memory: 43,248 KB -BaekjoonHub

---
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.py
@@ -0,0 +1,11 @@
+T = int(input())
+
+for i in range(1, T+1):
+    num = int(input())
+    total = 0
+    for j in range(1, num+1):
+        if j % 2 == 1:
+            total += j
+        else:
+            total -= j
+    print(f'#{i} {total}')
