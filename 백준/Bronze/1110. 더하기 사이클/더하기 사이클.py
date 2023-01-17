cae584ad822e0175a8dba384c9465f05e9be35cb
조정곤 <121420298+JeonggonCho@users.noreply.github.com>
2023-01-17 17:56:20 +0900

    [Bronze I] Title: 더하기 사이클, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,14 +1,12 @@
 N = int(input())
 cycle = 0
-
-if N >= 0 and N <= 9:
-    N = N * 10
-
 N1 = N
 
 while True:
+    A = N1 % 10
+    B = N1 // 10
+    N1 = (A * 10) + ((A + B) % 10)
     cycle += 1
-    N1 = ((N1 % 10) * 10) + (((N1 // 10) + (N1 % 10)) % 10)
     if N == N1:
         break
 print(cycle)
