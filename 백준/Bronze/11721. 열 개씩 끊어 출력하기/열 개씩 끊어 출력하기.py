8c21c0dbb0524268a12df47e8125798dd0922f18
조정곤 <121420298+JeonggonCho@users.noreply.github.com>
2023-01-18 18:01:11 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.py
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.py
@@ -1,10 +1,8 @@
-import math
+char = input() # 단어입력받기
 
-char = input()
-a = math.ceil(len(char) / 10)
-cnt = 0
-
-for i in range(a):
-    cnt += 1
-    b = cnt * 10
-    print(char[(b-10):b])
+while True: # 계속 실행하기
+    print(char[0:10]) # 앞부분 10개 출력
+    char = char.replace(char[0:10], '') # 앞부분 10개 삭제
+    
+    if len(char) == 0: # 출력할 문자가 없으면 멈춤
+        break
