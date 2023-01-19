a92b0b097a1911e4b7125017036ee9a3ae67b471
조정곤 <121420298+JeonggonCho@users.noreply.github.com>
2023-01-19 16:58:22 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 244 ms, Memory: 48056 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,9 +1,12 @@
-n = int(input())
+import sys
+
+n = int(sys.stdin.readline())
 my_dict = {}
 
 for i in range(n):
-    a, b = input().split()
+    a, b = sys.stdin.readline().split()
     my_dict[a] = b
+
 for j in sorted(my_dict)[::-1]:
     if my_dict.get(j) == 'enter':
         print(j)
