5ad137847dacc3aea75e6a484b0f24ecad3f434a
조정곤 <121420298+JeonggonCho@users.noreply.github.com>
2023-01-26 11:04:18 +0900

    [Silver IV] Title: 제로, Time: 4720 ms, Memory: 31248 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,10 @@
+K = int(input())
+num_list = []
+
+for i in range(K):
+    num = int(input())
+    if num != 0:
+        num_list.append(num)
+    else:
+        num_list.pop()
+print(sum(num_list))
