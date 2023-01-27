a52b8768b98349ceb80fa9be2ba03275dc16a4ed
조정곤 <121420298+JeonggonCho@users.noreply.github.com>
2023-01-27 23:51:32 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -0,0 +1,22 @@
+def solution(keyinput, board):
+    answer = [0, 0]
+    
+    for i in keyinput:
+        if i == 'up':
+            if ((board[1] - 1) / 2) <= answer[1]:
+                continue
+            answer[1] += 1
+        elif i == 'down':
+            if -((board[1] - 1) / 2) >= answer[1]:
+                continue
+            answer[1] -= 1
+        elif i == 'left':
+            if -((board[0] - 1) / 2) >= answer[0]:
+                continue
+            answer[0] -= 1
+        elif i == 'right':
+            if ((board[0] - 1) / 2) <= answer[0]:
+                continue
+            answer[0] += 1
+            
+    return answer
