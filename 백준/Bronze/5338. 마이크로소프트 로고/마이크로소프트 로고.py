30ee26d5ccde081d73941d5c0ab9dd1a6452bea0
조정곤 <121420298+JeonggonCho@users.noreply.github.com>
2023-02-08 22:05:09 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.py
@@ -0,0 +1,7 @@
+char = "       _.-;;-._"
+char1 = "'-..-'|   ||   |"
+char2 = "'-..-'|_.-;;-._|"
+char3 = "'-..-'|   ||   |"
+char4 = "'-..-'|_.-''-._|"
+
+print(char, char1, char2, char3, char4, sep='\n')
