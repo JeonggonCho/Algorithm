4eb51045c58353f5978be3c12929956b3eb07904
조정곤 <121420298+JeonggonCho@users.noreply.github.com>
2023-04-07 22:42:04 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -0,0 +1,6 @@
+def solution(citations):
+    citations.sort(reverse=True)
+    for index , citation in enumerate(citations):
+        if index >= citation:
+            return index
+    return len(citations)
