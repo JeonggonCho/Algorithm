1d697012d7ee87704d8e5ee20a411ffb03fb99c5
조정곤 <wjdrhs62@gmail.com>
2023-05-21 16:24:23 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -0,0 +1,9 @@
+function solution(arr, delete_list) {
+    var answer = [];
+    for (const i of arr) {
+        if (! delete_list.includes(i)) {
+            answer.push(i)
+        }
+    }
+    return answer;
+}
