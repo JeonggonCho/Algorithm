264ec85954f4c03c064fe87dcfd4a343695f9c01
조정곤 <wjdrhs62@gmail.com>
2023-06-21 13:13:59 +0900

    [unrated] Title: 글자 지우기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
@@ -0,0 +1,9 @@
+function solution(my_string, indices) {
+    var answer = '';
+    for (i = 0; i < my_string.length; i++) {
+        if (! indices.includes(i)) {
+            answer += my_string.charAt(i);
+        }
+    }
+    return answer;
+}
