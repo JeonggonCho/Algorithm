ddb46f57e5952e2150ebda41c7b54043e34ed7ec
조정곤 <wjdrhs62@gmail.com>
2023-08-18 21:40:31 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,15 +1,3 @@
-// function solution(weights) {
-//     var answer = 0;
-//     var len = weights.length;
-//     for (let i = 0; i < len; i++) {
-//         var weight = weights.pop();
-//         var weights2 = weights.map((x) => x * 2);
-//         var weights3 = weights.map((x) => x * 3);
-//         var weights4 = weights.map((x) => x * 4);
-//     }
-//     return answer;
-// }
-
 function solution(weights) {
     var answer = 0;
     const weight = {};
