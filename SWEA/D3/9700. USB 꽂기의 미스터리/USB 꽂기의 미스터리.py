3bd43116c95171748343b6a7546730f82d00f28c
조정곤 <wjdrhs62@gmail.com>
2023-09-20 05:03:21 +0900

    [D3] Title: USB 꽂기의 미스터리, Time: 109 ms, Memory: 42,728 KB -BaekjoonHub

---
+++ SWEA/D3/9700. USB 꽂기의 미스터리/USB 꽂기의 미스터리.py
@@ -0,0 +1,8 @@
+T = int(input())
+
+for i in range(1, T+1):
+    p, q = map(float, input().split())
+    if (1 - p) * q < (p * (1 - q)) * q:
+        print(f'#{i} YES')
+    else:
+        print(f'#{i} NO')
