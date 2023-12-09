18dbea3a7b630726efc660a5ef7590ab09c6c202
조정곤 <wjdrhs62@gmail.com>
2023-12-09 22:58:35 +0900

    [Silver II] Title: 쇠막대기, Time: 68 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -1,26 +1,18 @@
 import sys
-from collections import deque
 
-char = deque(sys.stdin.readline().strip())
-
-li = []
-while char:
-    if char[0] == "(" and char[1] == ")":
-        li.append("l")
-        char.popleft()
-        char.popleft()
-    else:
-        li.append(char.popleft())
+char = sys.stdin.readline().strip()
 
 result = 0
 temp = 0
-for i in range(len(li)):
-    if li[i] == "(":
-        temp += 1
-    elif li[i] == ")":
-        temp -= 1
-        result += 1
-    elif li[i] == "l":
-        result += temp
+for i in range(len(char) - 1):
+    if char[i] == "(":
+        if char[i + 1] == ")":
+            result += temp
+        elif char[i + 1] == "(":
+            temp += 1
+    elif char[i] == ")":
+        if char[i + 1] == ")":
+            temp -= 1
+            result += 1
 
 print(result)
