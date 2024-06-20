ba66ef6d90c7ed951f684fa8cb8baaf308c94ad4
조정곤 <wjdrhs62@gmail.com>
2024-06-20 17:54:36 +0900

    [level 2] Title: 호텔 대실, Time: 5.03 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -0,0 +1,29 @@
+function solution(book_time) {
+    const convertMinute = (a) => a[0] * 60 + a[1];
+    
+    const convertedTime = book_time.map((time) => {
+        let start = convertMinute(time[0].split(":").map(Number));
+        let end = convertMinute(time[1].split(":").map(Number));
+        end += 10;
+        return [start, end];
+    });
+    
+    convertedTime.sort((a, b) => a[0] - b[0]);
+    
+    let rooms = [];
+    for (let i = 0; i < convertedTime.length; i++) {
+        let flag = false;
+        for (let j = 0; j < rooms.length; j++) {
+            if (rooms[j] <= convertedTime[i][0]) {
+                rooms[j] = convertedTime[i][1];
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            rooms.push(convertedTime[i][1]);
+        }
+    }
+    
+    return rooms.length;
+}
