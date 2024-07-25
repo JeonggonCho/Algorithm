f26b3c9b47e32e6b3e811dee5e8fd9188daac978
조정곤 <wjdrhs62@gmail.com>
2024-07-25 14:31:50 +0900

    [Silver III] Title: N과 M (1), Time: 344 ms, Memory: 40956 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -0,0 +1,38 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString();
+
+const [n, m] = input.split(' ').map(Number);
+
+let arr = [];
+for (let i = 1; i <= n; i++) {
+  arr.push(i);
+}
+
+let visited = new Array(n).fill(false);
+let selected = [];
+
+let answer = '';
+const dfs = (arr, depth) => {
+  if (depth === m) {
+    let result = [];
+    for (let j of selected) {
+      result.push(arr[j]);
+    }
+    for (let k of result) {
+      answer += k + ' ';
+    }
+    answer += '\n';
+  }
+  for (let l = 0; l < arr.length; l++) {
+    if (visited[l]) continue;
+    selected.push(l);
+    visited[l] = true;
+    dfs(arr, depth + 1);
+    selected.pop();
+    visited[l] = false;
+  }
+};
+
+dfs(arr, 0);
+
+console.log(answer);
